382a73b7bdf891009b623558bf235c11ae810b82
allenshaji17 <allenshaji.c@protonmail.com>
2021-07-12 23:52:25 +0530

    personal details program

---
+++ personal_details.py
@@ -0,0 +1,11 @@
+personal_details={
+	"name":"allen",
+	"age":21,
+	"contact":6238326285,
+	"address":{
+		"city":"kottayam",
+		"landmark":"Marthoma church",
+		"po":686541
+	}
+}
+print(personal_details)
