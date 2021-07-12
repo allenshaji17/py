56a31b3f9ea658f6063dd683d48ba26d9cb0e851
allenshaji17 <allenshaji.c@protonmail.com>
2021-07-12 23:50:33 +0530

    initial vegetables program\

---
+++ vegetables.py
@@ -0,0 +1,5 @@
+vegetables=["cucumber","carrot","potato","eggplant","brocoli"]
+print("first vegetable:",vegetables[0])
+print ("last middle three vegetables:",vegetables[1:4])
+vegetables[3]="tomato"
+print(vegetables)
