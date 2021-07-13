6e2c6c52ef71f2e5b6b0b3d0253d6d40860a0046
allenshaji17 <allenshaji.c@protonmail.com>
2021-07-13 21:41:56 +0530

    fixed com[;etely

--- marks.py
+++ marks.py
@@ -1,18 +1,19 @@
-marks=[84,82,89,82,89]
+marks=[99,99,99,99,44]
 Sum = 0
-for i in range(0, len(marks)):
+for i in range(0,len(marks)):
     Sum = Sum + marks[i]
-avg = Sum/5
-if (avg>75):
-	print("distinction")
-else:
-    for i in marks:
-        if (i < 45):
-            print("failed")
-            break
-    else:
-        print("passed")
+avg = Sum/len(marks)
+fail = False
+for i in marks:
+    if (i < 45):
+        fail = True
+        print("failed")
 
+if (avg > 75) and fail != True:
+    print("distinction")
+else:
+    print("passed")
+    
 
 
 
